b148d809193a0e0ad628174721aee9f536c6a97d
proBaxia <oladimejiakintade0224@outlook.com>
2022-12-21 11:02:14 +0100

    update-vpc

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ variable  subnet_cidr_block {}
 variable avali_zone {}
 variable "env_prefix" {}
 
-
+#create vpc = my-app-vpc
 resource "aws_vpc" "my-app-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
       Name:"${var.env_prefix}-vpc"
   }
 }
-
+#create subnet myapp-subnet-1
 
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id     = aws_vpc.my-app-vpc.id
@@ -24,6 +24,8 @@ resource "aws_subnet" "myapp-subnet-1" {
      Name = "${var.env_prefix}-subnet-1"
   }
 }
+
+# create route_route_table = my-app-route-table
 resource "aws_route_table" "my-app-route-table" {
   vpc_id = aws_vpc.my-app-vpc.id
 
@@ -35,6 +37,7 @@ resource "aws_route_table" "my-app-route-table" {
      Name: "${var.env_prefix}-rtb"
   } 
 }
+#create internet_gateway = myapp-igw
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.my-app-vpc.id
   tags = {
@@ -42,6 +45,7 @@ resource "aws_internet_gateway" "myapp-igw" {
   }
   
 }
+#create aws_route_table_association = a-rtb-subnet-1
 resource "aws_route_table_association" "a-rtb-subnet-1" {
   subnet_id      = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.my-app-route-table.id
